2d004b28e453ac9b11f64c584991588fb1b10a45
Plum-Tree-Garden <73845528+Plum-Tree-Garden@users.noreply.github.com>
2021-08-06 15:22:24 -0400

    Create main.java

---
+++ typingTutor/main.java
@@ -0,0 +1,11 @@
+// Plum Tree Garden
+// main.java
+
+public class Main 
+{	
+	public static void main(String[] args) 
+	{
+		// construct new typingtutor object
+		TypingTutor typingTutor = new TypingTutor();
+	}
+}
